8d2f90fd8521282e35de7df901cf00dc84e52030
littla2liyou <1446638814@qq.com>
2025-09-18 15:59:38 +0800

    component

---
+++ packages/runtime-renderer/src/components/PageRenderer.vue
@@ -0,0 +1,16 @@
+<template>
+  <SchemaRenderer :schema="currentSchema" />
+</template>
+
+<script setup>
+import SchemaRenderer from '../renderer/RenderMain'
+import { useRoute } from 'vue-router'
+import { computed } from 'vue'
+
+const route = useRoute()
+
+// 直接使用路由 meta 中的页面 schema
+const currentSchema = computed(() => {
+  return (route.meta && route.meta.pageSchema) || {}
+})
+</script>
