1f4b5baa1dfeb52d5bbdbadff5f609ee6503866e
littla2liyou <1446638814@qq.com>
2025-09-18 19:25:15 +0800

    Add key 用于强制刷新组件

--- packages/runtime-renderer/src/App.vue
+++ packages/runtime-renderer/src/App.vue
@@ -1,31 +1,16 @@
 <template>
-  <SchemaRenderer :schema="appSchema" />
+  <router-view :key="$route.fullPath" />
 </template>
 
 <script setup>
-import SchemaRenderer from './renderer/RenderMain'
-import mockSchema from './mock/page1.json'
 import { useAppSchema } from './composables/useAppSchema'
 
-// 直接初始化 schema，不需要异步加载
-const appSchema = mockSchema
-const { initializeAppConfig, pages, globalStates, fetchAppSchema, fetchBlocks } = useAppSchema()
+const { fetchBlocks } = useAppSchema()
 
 // 异步加载数据
 const loadAppData = async () => {
-  await fetchAppSchema()
   await fetchBlocks()
-
-  // 数据加载完成后访问计算属性的值
-  // eslint-disable-next-line no-console
-  console.log('页面数据:', pages.value)
-  // eslint-disable-next-line no-console
-  console.log('全局状态:', globalStates.value)
-
-  // 初始化应用配置
-  initializeAppConfig(appSchema)
 }
 
-// 调用异步加载函数
 loadAppData()
 </script>
