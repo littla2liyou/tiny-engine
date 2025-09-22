cffcebb76e8e33eea915836a440b03aa1eaa7657
littla2liyou <1446638814@qq.com>
2025-09-22 20:14:17 +0800

    fix: 深拷贝state并删除stores的reactive包装

--- packages/runtime-renderer/src/stores/index.ts
+++ packages/runtime-renderer/src/stores/index.ts
@@ -1,13 +1,12 @@
 import { defineStore } from 'pinia'
 import type { StoreConfig } from '../types/config'
 import type { Pinia } from 'pinia'
-import { reactive } from 'vue'
 
 // 存储已创建的 stores 实例
 const storeInstances = new Map<string, any>()
 
 export const createStores = (storesConfig: StoreConfig[], pinia: Pinia) => {
-  const stores = reactive<Record<string, any>>({}) // 使用 reactive 包装 stores
+  const stores = <Record<string, any>>{}
 
   storesConfig.forEach((config) => {
     // 检查是否已经创建过该 store
@@ -18,22 +17,19 @@ export const createStores = (storesConfig: StoreConfig[], pinia: Pinia) => {
 
     // 使用 defineStore 创建 Pinia store
     const useStore = defineStore(config.id, {
-      state: () => ({ ...config.state }),
+      state: () => JSON.parse(JSON.stringify(config.state)),
 
       getters: config.getters,
 
       actions: config.actions
     })
 
-    // 创建 store 实例并绑定到 pinia
+    // 使用useStore创建 store 实例并绑定到 pinia
     const storeInstance = useStore(pinia)
 
     // 缓存创建的 store 实例
     storeInstances.set(config.id, storeInstance)
     stores[config.id] = storeInstance
-
-    // eslint-disable-next-line no-console
-    console.log(`Store ${config.id} 已创建`)
   })
 
   return stores
