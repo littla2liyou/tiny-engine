116c01ed489238ed278c2933573773c4e761422b
littla2liyou <1446638814@qq.com>
2025-09-24 19:24:01 +0800

    Update CanvasRouterView.vue

--- packages/runtime-renderer/src/renderer/builtin/CanvasRouterView.vue
+++ packages/runtime-renderer/src/renderer/builtin/CanvasRouterView.vue
@@ -1,25 +1,11 @@
 <template>
-  <router-view />
+  <div class="canvas-container">
+    <router-view />
+  </div>
 </template>
-<!-- <script lang="ts">
-import { inject } from 'vue'
-import CanvasPlaceholder from './CanvasPlaceholder.vue'
-export default {
-  components: {
-    CanvasPlaceholder
-  },
-  props: {
-    name: {
-      type: String
-    },
-    params: {
-      type: Object
-    }
-  },
-  setup() {
-    const pageId = inject('pageContext')?.pageId
 
-    return { pageId }
-  }
+<style lang="less" scoped>
+.canvas-container {
+  min-height: 48px;
 }
-</script> -->
+</style>
